bcb4b17047f78666fc47334ecdebb2b116c326d6
Navneet Kaur <kaur2305navneet@gmail.com>
2022-12-26 11:16:39 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 const http = require('http');
 
-const server = http.createServer(function(req,res){
-    console.log(req.url,req.method,req.headers);
+const server = http.createServer((req,res)=>{
+    const url = req.url;
+    if(url == '/home')
+    {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Project</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+         res.write('</html>'); 
+         return  res.end();
+
+    }
+     if(url == '/about')
+    {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>>My First Project</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+         res.write('</html>'); 
+         return  res.end();
+    }
+     if(url == '/node')
+    {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>>My First Project</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+         res.write('</html>'); 
+         return  res.end();
+    }
+
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>My First Project</title></head>');
