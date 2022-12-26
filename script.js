d594c644cd261ee745e38f2ea39f2e1b25db3fca
Navneet Kaur <kaur2305navneet@gmail.com>
2022-12-26 19:54:32 +0530

    first commit

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
     const url = req.url;
-    if(url == '/home')
+    const method = req.method;
+    if(url == '/')
     {
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<head><title>My First Project</title></head>');
-        res.write('<body><h1>Welcome home</h1></body>');
-         res.write('</html>'); 
+        fs.readFile("message.txt",{encoding:"utf-8" } , (err,data)=>{
+            if(err)
+            {
+                console.log(err);
+            }
+            console.log('data from file'+data);
+            res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write(`<body>${data}</body>`);
+        res.write('<body><form action = "/message" method = "POST"><input type = "text" name = "message"><button type = "submit">SEND</button> </form></body>');
+         res.write('</html>');    
          return  res.end();
-
+        });
+             
     }
-     if(url == '/about')
+    if(url === '/message' && method === 'POST')
     {
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<head><title>>My First Project</title></head>');
-        res.write('<body><h1>Welcome to About Us page</h1></body>');
-         res.write('</html>'); 
-         return  res.end();
+        const body=[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk); 
+        });
+         req.on('end',()=>
+         {
+             const parsedbody = Buffer.concat(body).toString();
+             const message = parsedbody.split('=')[1];
+             fs.writeFileSync('message.txt', message);
+             res.statusCode= 302;
+             res.setHeader('Location','/');
+             return res.end(); 
+         });
+        
+        
+        
     }
-     if(url == '/node')
-    {
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<head><title>>My First Project</title></head>');
-        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
-         res.write('</html>'); 
-         return  res.end();
-    }
-
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Project</title></head>');
-    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
-     res.write('</html>');
-     res.end();
+     
+    // res.setHeader('Content-Type','text/html');
+    // res.write('<html>');
+    // res.write('<head><title>My First Project</title></head>');
+    // res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    //  res.write('</html>');
+    //  res.end();
 
 });
 
